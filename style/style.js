949d1d495222934c8d0724fdc4824d9872753790
liisapikkarainen <90195292+liisapikkarainen@users.noreply.github.com>
2023-02-13 14:10:09 +0200

    Add files via upload

---
+++ style/style.js
@@ -0,0 +1,59 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  bac: {
+    marginTop: 50,
+    fontSize: 30,
+    paddingLeft: 5,
+    textAlign: 'center',
+  },
+  weight: {
+    backgroundColor: "#a1e0ff",
+    marginTop: 30,
+    fontSize: 25,
+    paddingLeft: 5,
+  },
+  values: {
+    backgroundColor: '#c9c9c953',
+    textAlign: 'left',
+    fontSize: 30,
+    paddingLeft: 5,
+  },
+  titles: {
+    backgroundColor: "#a1e0ff",
+    textAlign: 'left',
+    fontSize: 25,
+    paddingLeft: 5,
+  },
+  gender: {
+    textAlign: 'center',
+    fontSize: 25,
+    paddingLeft: 5,
+    marginTop: 25,
+  },
+  /* Male and female buttons */ 
+  genderButton: {
+    padding: 20,
+    alignSelf: 'center',
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    backgroundColor: "#a1e0ff7d",
+    width: 150,
+  },
+  calculate: {
+    padding: 10,
+    width: 200,
+    fontSize: 30,
+    marginTop: 20,
+    alignSelf: 'center',
+    textAlign: 'center',
+    backgroundColor: "#03a7f9b0",
+  },
+  result: {
+    padding: 10,
+    marginTop: 20,
+    fontSize: 50,
+    textAlign: 'center',
+  },
+});
